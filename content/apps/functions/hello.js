8e2cbdd6f07da3831786e00e347ff944c12c3831
Oliver Schmid <oliver.schmd@gmail.com>
2019-03-02 14:13:11 -0800

    remove unused gitmodules, add googleapis dependency

--- content/apps/functions/hello.js
+++ content/apps/functions/hello.js
@@ -1,6 +1,9 @@
+const {google} = require('googleapis');
+
 exports.handler = function(event, context, callback) {
     callback(null, {
         statusCode: 200,
-        body: "Hello, World\n\nprocess.env.TEST_VAR:" + process.env.TEST_VAR
+        body: "Hello, World\n\nprocess.env.TEST_VAR:" + process.env.TEST_VAR +
+            "\n\ngoogle:" + JSON.stringify(google)
     });
 }
