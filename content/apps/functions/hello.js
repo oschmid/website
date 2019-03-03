f0c7b8f2f9ca36aacc8bcec53b4a02f6652788fe
Oliver Schmid <oliver.schmd@gmail.com>
2019-03-03 11:58:19 -0800

    print methods

--- content/apps/functions/hello.js
+++ content/apps/functions/hello.js
@@ -1,9 +1,23 @@
 import {calendar_v3} from 'googleapis';
 
+function getMethods(obj) {
+  var result = [];
+  for (var id in obj) {
+    try {
+      if (typeof(obj[id]) == "function") {
+        result.push(id + ": " + obj[id].toString());
+      }
+    } catch (err) {
+      result.push(id + ": inaccessible");
+    }
+  }
+  return result;
+}
+
 export function handler(event, context, callback) {
     callback(null, {
         statusCode: 200,
         body: "Hello, World\n\nprocess.env.TEST_VAR:" + process.env.TEST_VAR +
-            "\n\ngoogle:" + JSON.stringify(calendar_v3)
+            "\n\ncalendar_v3.methods:" + getMethods(calendar_v3).join(',')
     });
 }
