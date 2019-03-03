91a52dceddccae0f946b1754c58dac2755ad1403
Oliver Schmid <oliver.schmd@gmail.com>
2019-03-03 14:10:08 -0800

    fix undefined auth

--- content/apps/functions/sailor-jacks.js
+++ content/apps/functions/sailor-jacks.js
@@ -1,11 +1,13 @@
 import {google} from 'googleapis';
 
 export function handler(event, context, callback) {
-    const oAuth2Client = new google.auth.OAuth2(
+    console.log("google:" + google);
+    console.log("google.auth:" + google.auth);
+
+    const auth = new google.auth.OAuth2(
         process.env.GOOGLE_CALENDAR_CLIENT_ID,
         process.env.GOOGLE_CALENDAR_SECRET,
         ["https://oliverschmid.space/"]);
-
 
     const calendar = google.calendar({version: 'v3', auth});
     callback(null, {
