36f4bb09e2c734a34a72f5d7578f16443b58f545
Oliver Schmid <oliver.schmd@gmail.com>
2019-03-03 11:22:14 -0800

    import calendar api

--- content/apps/functions/hello.js
+++ content/apps/functions/hello.js
@@ -1,9 +1,9 @@
-import google from 'googleapis';
+import {calendar} from 'googleapis';
 
 export function handler(event, context, callback) {
     callback(null, {
         statusCode: 200,
         body: "Hello, World\n\nprocess.env.TEST_VAR:" + process.env.TEST_VAR +
-            "\n\ngoogle:" + JSON.stringify(google)
+            "\n\ngoogle:" + JSON.stringify(calendar)
     });
 }
