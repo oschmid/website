f32c9e6ae205c373e270e12230ed85a4bad91742
Oliver Schmid <oliver.schmd@gmail.com>
2019-03-03 11:32:10 -0800

    stringify calendar_v3

--- content/apps/functions/hello.js
+++ content/apps/functions/hello.js
@@ -4,6 +4,6 @@ export function handler(event, context, callback) {
     callback(null, {
         statusCode: 200,
         body: "Hello, World\n\nprocess.env.TEST_VAR:" + process.env.TEST_VAR +
-            "\n\ngoogle:" + JSON.stringify(calendar)
+            "\n\ngoogle:" + JSON.stringify(calendar_v3)
     });
 }
