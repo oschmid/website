8b668014d7567b2a4963bb0e59db3d8f1dfd1611
Oliver Schmid <oliver.schmd@gmail.com>
2021-11-12 14:23:13 -0800

    Use default /netlify/functions dir; Use menu login widget

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,7 +1,7 @@
 exports.handler = async (event, context) => {
   const { user } = context.clientContext;
   if (!user) {
-    return { statusCode: 401 };
+    return { statusCode: 401, body: JSON.stringify(context.clientContext) };
   }
   return {
     statusCode: 200,
