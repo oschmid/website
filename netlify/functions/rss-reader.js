80465df8ae26f27391fc9379408296664b796010
Oliver Schmid <oliver.schmd@gmail.com>
2022-05-11 07:17:54 -0700

    Render basic HTML template of blog titles using mustache

--- netlify/functions/rss-reader.js
+++ netlify/functions/rss-reader.js
@@ -1,9 +1,18 @@
+import Mustache from "mustache";
 import Parser from "rss-parser";
 const parser = new Parser();
 
 const FEEDS = [ "https://astralcodexten.substack.com/feed" ];
+const PAGE_TEMPLATE = `
+<!DOCTYPE html>
+<ul>
+{{#items}}
+  <li>{{title}}
+{{/items}}
+</ul>`;
 
 exports.handler = async (event, context) => {
   let feed = await parser.parseURL(FEEDS[0]);
-  return { statusCode: 200, body: JSON.stringify(feed.items) };
+  let page = Mustache.render(PAGE_TEMPLATE, feed);
+  return { statusCode: 200, body: page };
 };
