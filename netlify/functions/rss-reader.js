b9c47eaed3b710653811bf2a33cfbfbeb89cd181
Oliver Schmid <oliver.schmd@gmail.com>
2022-05-29 20:31:39 -0700

    Add header and footer

--- netlify/functions/rss-reader.js
+++ netlify/functions/rss-reader.js
@@ -17,20 +17,44 @@ const PAGE_TEMPLATE = `
         display: block;
       }
     </style>
-    <main class="container is-readable-column">
-    {{#items}}
-      <article id="{{postUrl}}" class="section">
-        <header>
-          <h2><a class="title is-plain" href="{{postUrl}}">{{title}}</a></h2>
+    <div class="section">
+      <div class="container">
+        <header class="title">
+          <h1><a class="is-plain" href="/">Oliver Schmid</a></h1>
         </header>
-        <p class="subtitle has-text-grey"><a class="has-text-grey" href="{{siteUrl}}">{{author}}</a>&nbsp;\/\/ {{displayDate}} at {{displayTime}}&nbsp;</span></p>
-        <p class="is-plain">{{{body}}}</p>
-      </article>
-    {{/items}}
-    <nav class="pagination is-centered">
-      <button id="previous" class="button pagination-previous">Previous</button>
-      <button id="next" class="button pagination-next">Next</button>
-    </nav>
+      </div>
+      <main class="container is-readable-column">
+      {{#items}}
+        <article id="{{postUrl}}" class="section">
+          <header>
+            <h2><a class="title is-plain" href="{{postUrl}}">{{title}}</a></h2>
+          </header>
+          <p class="subtitle has-text-grey"><a class="has-text-grey" href="{{siteUrl}}">{{author}}</a>&nbsp;\/\/ {{displayDate}} at {{displayTime}}&nbsp;</span></p>
+          <p class="is-plain">{{{body}}}</p>
+        </article>
+      {{/items}}
+      <nav class="pagination is-centered">
+        <button id="previous" class="button pagination-previous">Previous</button>
+        <button id="next" class="button pagination-next">Next</button>
+      </nav>
+    </div>
+    <footer class="footer">
+      <div class="content has-text-centered">
+        <a class="is-plain" href="/">Home</a>
+        <a class="is-plain is-spaced-footer-link rss-button" title="Subscribe to RSS" href="/index.xml">
+          <svg xmlns="http://www.w3.org/2000/svg" class="rss-icon" viewBox="0 0 8 8" width="22" height="22">
+            <title>Subscribe to RSS</title>
+            <circle cx="2" cy="6" r="1"></circle>
+            <path d="m 1,4 a 3,3 0 0 1 3,3 h 1 a 4,4 0 0 0 -4,-4 z"></path>
+            <path d="m 1,2 a 5,5 0 0 1 5,5 h 1 a 6,6 0 0 0 -6,-6 z"></path>
+          </svg>Subscribe</a>
+          <a class="is-plain is-spaced-footer-link" href="/about/">About</a>
+          <a class="is-plain is-spaced-footer-link" href="/contact/">Contact</a>
+        <p>
+        <div>©2022 Oliver Schmid</div>
+        <p>
+      </div>
+    </footer>
     </main>
     <script>
       // initialize state
