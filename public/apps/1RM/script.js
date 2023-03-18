810033418fdb10c89e459201bc673791540fa59b
Oliver Schmid <oliver.schmd@gmail.com>
2023-03-17 22:52:11 -0700

    Add Conjugate Method Efforts

--- public/apps/1RM/script.js
+++ public/apps/1RM/script.js
@@ -1,6 +1,9 @@
-const onerms = [];
-const percentages = [];
-const values = []; // contains either {weight:10} or {percent:10}
+const values = []; // contains either {weight:10} or {percent:10} TODO localStorage
+const explosiveEfforts = [ 20, 23, 27 ];
+const dynamicEfforts = [ 25, 50, 55, 60 ];
+const repetitiveEfforts = [ 68.5, 73.5, 75, 78.5, 83.5 ];
+const maxEfforts = [ 85, 93.5 ];
+var efforts = 0; // 0=all, 1=explosive, 2=dynamic, 3=repetitive, 4=max, 5=custom TODO localStorage
 
 window.onload = () => {
     const onermElement = document.getElementById("onerm");
@@ -22,7 +25,7 @@ window.onload = () => {
         }
     };
     addPercentageElement.onclick = () => {
-        values.push({percent: percentageElement.value});
+        values.push({percent: parseInt(percentageElement.value)});
         percentageElement.value = "";
         render();
         percentageElement.focus();
@@ -44,6 +47,30 @@ window.onload = () => {
         values.length = 0;
         render();
     };
+    document.getElementById("all-efforts").onclick = () => {
+        efforts = 0;
+        render();
+    };
+    document.getElementById("explosive-efforts").onclick = () => {
+        efforts = 1;
+        render();
+    };
+    document.getElementById("dynamic-efforts").onclick = () => {
+        efforts = 2;
+        render();
+    };
+    document.getElementById("repetitive-efforts").onclick = () => {
+        efforts = 3;
+        render();
+    };
+    document.getElementById("max-efforts").onclick = () => {
+        efforts = 4;
+        render();
+    };
+    document.getElementById("custom-efforts").onclick = () => {
+        efforts = 5;
+        render();
+    };
     const render = () => {
         var content = "<tr><th></th>";
         const onerms = values.filter(v => 'weight' in v).map(v => v.weight);
@@ -51,18 +78,35 @@ window.onload = () => {
             content += "<th>" + onerm + "</th>";
         }
         content += "</tr>";
-        for (v of values) {
-            if (!('percent' in v)) {
-                continue;
-            }
-            content += "<tr><th>" + v.percent + "%</th>";
+        var percentages = values.filter(v => 'percent' in v).map(v => v.percent);
+        switch (efforts) {
+            case 0:
+                percentages = percentages.concat(explosiveEfforts, dynamicEfforts, repetitiveEfforts, maxEfforts);
+                break;
+            case 1:
+                percentages = percentages.concat(explosiveEfforts);
+                break;
+            case 2:
+                percentages = percentages.concat(dynamicEfforts);
+                break;
+            case 3:
+                percentages = percentages.concat(repetitiveEfforts);
+                break;
+            case 4:
+                percentages = percentages.concat(maxEfforts);
+                break;
+        }
+        percentages.sort((a, b) => a - b);
+        for (percent of percentages) {
+            content += "<tr><th>" + percent + "%</th>";
             for (onerm of onerms) {
-                content += "<td>" + (onerm * v.percent / 100) + "</td>";
+                content += "<td>" + (onerm * percent / 100) + "</td>";
             }
             content += "</tr>"
         }
         tableElement.innerHTML = content;
     };
+    render();
 
     if ('serviceWorker' in navigator) {
         navigator.serviceWorker.register('./service-worker.js');
