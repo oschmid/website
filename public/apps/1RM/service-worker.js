818a583ee657e95082c96118e826f76073c0f9b2
Oliver Schmid <oliver.schmd@gmail.com>
2023-03-20 07:29:35 -0700

    Make service-worker try network before cache

--- public/apps/1RM/service-worker.js
+++ public/apps/1RM/service-worker.js
@@ -1,18 +1,45 @@
+const CACHE_NAME = 'v2';
+
 self.addEventListener('install', (event) => {
     event.waitUntil(
-        caches.open('v1').then((cache) => {
+        caches.open(CACHE_NAME).then((cache) => {
             return cache.addAll([
                 '/apps/1RM/index.html',
                 '/apps/1RM/script.js',
                 '/apps/1RM/weightlifting.png']);
         })
     );
+    self.skipWaiting();
 });
 
-self.addEventListener('fetch', function (event) {
-  event.respondWith(
-    caches.match(event.request).then(function (response) {
-      return response || fetch(event.request);
-    }),
-  );
+self.addEventListener("activate", (event) => {
+  event.waitUntil((async () => {
+      // Enable navigation preload if it's supported.
+      // See https://developers.google.com/web/updates/2017/02/navigation-preload
+      if ("navigationPreload" in self.registration) {
+        await self.registration.navigationPreload.enable();
+      }
+  })());
+
+  // Tell the active service worker to take control of the page immediately.
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith((async () => {
+    try {
+      const preloadResponse = await event.preloadResponse;
+      if (preloadResponse) {
+        return preloadResponse;
+      }
+
+      // Always try the network first.
+      return await fetch(event.request);
+    } catch (e) {
+      console.log("Fetch failed; returning offline page instead.", error);
+
+      const cache = await caches.open(CACHE_NAME);
+      return await cache.match(event.request);
+    }
+  })());
 });
