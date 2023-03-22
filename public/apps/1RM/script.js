20f2c04e152c18bd173ac5c6ec79911440c4a322
Oliver Schmid <oliver.schmd@gmail.com>
2023-03-21 19:18:57 -0700

    Use bulma for styling

--- public/apps/1RM/script.js
+++ public/apps/1RM/script.js
@@ -6,22 +6,22 @@ const maxEfforts = [ 85, 93.5 ];
 var efforts = 5; // 0=all, 1=explosive, 2=dynamic, 3=repetitive, 4=max, 5=custom TODO localStorage
 
 window.onload = () => {
-    const onermElement = document.getElementById("onerm");
+    const weightElement = document.getElementById("weight");
     const percentageElement = document.getElementById("percentage");
     const tableElement = document.getElementById("table");
-    const addOneRmElement = document.getElementById("add-onerm");
+    const addWeightElement = document.getElementById("add-weight");
     const addPercentageElement = document.getElementById("add-percentage");
 
-    addOneRmElement.onclick = () => {
-        values.push({weight: onermElement.value});
-        onermElement.value = "";
+    addWeightElement.onclick = () => {
+        values.push({weight: weightElement.value});
+        weightElement.value = "";
         render();
-        onermElement.focus();
+        weightElement.focus();
     };
-    onermElement.onkeyup = (event) => {
+    weightElement.onkeyup = (event) => {
         if (event.key === "Enter") {
             event.preventDefault();
-            addOneRmElement.click();
+            addWeightElement.click();
         }
     };
     addPercentageElement.onclick = () => {
@@ -72,16 +72,16 @@ window.onload = () => {
         render();
     };
     const render = () => {
-        var content = "<tr>";
-        const onerms = values.filter(v => 'weight' in v).map(v => v.weight);
+        var content = "<thead><tr>";
+        const weights = values.filter(v => 'weight' in v).map(v => v.weight);
         var percentages = values.filter(v => 'percent' in v).map(v => v.percent);
-        if (onerms.length > 0 && (efforts != 5 || percentages.length > 0)) {
+        if (weights.length > 0 && (efforts != 5 || percentages.length > 0)) {
             content += "<th></th>"
         }
-        for (onerm of onerms) {
-            content += "<th>" + onerm + "</th>";
+        for (weight of weights) {
+            content += "<th>" + weight + "</th>";
         }
-        content += "</tr>";
+        content += "</tr></thead><tbody>";
         switch (efforts) {
             case 0:
                 percentages = percentages.concat(explosiveEfforts, dynamicEfforts, repetitiveEfforts, maxEfforts);
@@ -102,11 +102,12 @@ window.onload = () => {
         percentages.sort((a, b) => a - b);
         for (percent of percentages) {
             content += "<tr><th>" + percent + "%</th>";
-            for (onerm of onerms) {
-                content += "<td>" + (onerm * percent / 100) + "</td>";
+            for (weight of weights) {
+                content += "<td>" + (weight * percent / 100) + "</td>";
             }
             content += "</tr>"
         }
+        content += "</tbody>";
         tableElement.innerHTML = content;
     };
     render();
