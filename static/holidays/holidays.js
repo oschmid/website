e92f13963f8164abda8f954f489670371c645754
Oliver Schmid <oliver.schmd@gmail.com>
2026-07-03 06:48:32 -0700

    Cleanup holidays.js

--- static/holidays/holidays.js
+++ static/holidays/holidays.js
@@ -1,67 +1,61 @@
-const today = new Date();
-
+function setLogo(src, title) {
+    var logo = document.getElementById('logo');
+    logo.src = src;
+    logo.title = title;
+}
+var today = new Date();
+//today = new Date(2026, 0, 1); // New Year's Test
+//today = new Date(2026, 6, 1); // Canada Day Test
+//today = new Date(2026, 7, 1); // 1. August Test
+//today = new Date(2026, 7, 31); // Malaysia Independence Test
+//today = new Date(2026, 8, 16); // Malaysia Day Test
+//today = new Date(2026, 8, 30); // Truth and Reconciliation Test
+//today = new Date(2026, 9, 12); // Thanksgiving Test
+//today = new Date(2026, 9, 31); // Halloween Test
+//today = new Date(2026, 10, 11); // Remembrance Day Test
+//today = new Date(2026, 11, 25); // Christmas Test
 switch (today.getMonth()) {
     case 0: // January
         if (today.getDate() === 1) {
-            var logo = document.getElementById('logo');
-            logo.src = '/holidays/fireworks.gif';
-            logo.title = 'Happy New Year!';
+            setLogo('/holidays/fireworks.avif', 'Happy New Year!');
         }
         break;
     case 6: // July
         if (today.getDate() === 1) {
-            var logo = document.getElementById('logo');
-            logo.src = '/holidays/Animated-Flag-Canada.gif';
-            logo.title = 'Happy Canada Day!';
+            setLogo('/holidays/Animated-Flag-Canada.gif', 'Happy Canada Day!');
         }
         break;
     case 7: // August
         if (today.getDate() === 1) {
-            var logo = document.getElementById('logo');
-            logo.src = '/holidays/Animated-Flag-Switzerland.gif';
-            logo.title = 'En schöne 1. August!';
+            setLogo('/holidays/Animated-Flag-Switzerland.gif', 'En schöne 1. August!');
         } else if (today.getDate() === 31) {
-            var logo = document.getElementById('logo');
-            logo.src = '/holidays/Animated-Flag-Malaysia.gif';
-            logo.title = 'Selamat Hari Merdeka!';
+            setLogo('/holidays/Animated-Flag-Malaysia.gif', 'Selamat Hari Merdeka!');
         }
         break;
     case 8: // September
         if (today.getDate() === 16) {
-            var logo = document.getElementById('logo');
-            logo.src = '/holidays/Animated-Flag-Malaysia.gif';
-            logo.title = 'Selamat Hari Malaysia!';
+            setLogo('/holidays/Animated-Flag-Malaysia.gif', 'Selamat Hari Malaysia!');
         } else if (today.getDate() === 30) {
-            var logo = document.getElementById('logo');
-            logo.src = '/holidays/orange-shirt.png';
-            logo.title = 'Truth and Reconciliation';
+            setLogo('/holidays/orange-shirt.png', 'Truth and Reconciliation');
         }
         break;
     case 9: // October
         var thanksgiving = new Date(today.getFullYear(), 9, 1);
         thanksgiving.setDate(thanksgiving.getDate() + ((8 - thanksgiving.getDay()) % 7 + 7)); // Calculate 2nd Monday
         if (today.getDate() === thanksgiving.getDate()) {
-            var logo = document.getElementById('logo');
-            logo.src = '/holidays/thanksgiving.png';
-            logo.title = 'Happy Thanksgiving!';
+            setLogo('/holidays/thanksgiving.png', 'Happy Thanksgiving!');
         } else if (today.getDate() === 31) {
-            var logo = document.getElementById('logo');
-            logo.src = '/holidays/pumpkin.gif';
-            logo.title = 'Happy Halloween!';
+            setLogo('/holidays/pumpkin.gif', 'Happy Halloween!');
         }
         break;
     case 10: // November
         if (today.getDate() === 11) {
-            var logo = document.getElementById('logo');
-            logo.src = '/holidays/poppy.png';
-            logo.title = 'Lest We Forget';
+            setLogo('/holidays/poppy.png', 'Lest We Forget');
         }
         break;
     case 11: // December
         if (today.getDate() === 25) {
-            var logo = document.getElementById('logo');
-            logo.src = '/holidays/christmas-tree.gif';
-            logo.title = 'Merry Christmas!';
+            setLogo('/holidays/christmas-tree.webp', 'Merry Christmas!');
         }
         break;
 }
