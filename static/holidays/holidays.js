94e65ffabe5be443ff3b1786c1a1ca21b06a50f3
Oliver Schmid <oliver.schmd@gmail.com>
2026-07-10 19:55:45 -0700

    Count days to next holiday

--- static/holidays/holidays.js
+++ static/holidays/holidays.js
@@ -1,32 +1,27 @@
-function setLogo(src, title) {
-    var logo = document.getElementById('logo');
-    logo.src = src;
-    logo.title = title;
+function daysBetween(date1, date2) {
+  const diffInMs = date2 - date1;
+  return Math.round(diffInMs / 86400000); // Number of milliseconds in one day: 24 * 60 * 60 * 1000
 }
-function isChineseNewYear(date) {
-    const jan21 = new Date(date.getFullYear(), 0, 21);
-    const feb20 = new Date(date.getFullYear(), 1, 20);
-    if (date < jan21 || date >= feb20) {
-        return false;
-    }
-    const newYear = Lunar.fromYmd(date.getFullYear(), 1, 1).getSolar();
-    return (date.getMonth() + 1) == newYear.getMonth() && date.getDate() == newYear.getDay();
+function getZodiacAnimal(year) {
+    const animals = ['monkey', 'rooster', 'dog', 'pig', 'rat', 'ox', 'tiger', 'rabbit', 'dragon', 'snake', 'horse', 'goat'];
+    return '/holidays/zodiac/' + animals[year % 12] + '.png';
 }
-function getZodiacAnimal(date) {
-    const animals = ['rat', 'ox', 'tiger', 'rabbit', 'dragon', 'snake', 'horse', 'goat', 'monkey', 'rooster', 'dog', 'pig'];
-    return "/holidays/zodiac/" + animals[date.getYear() % 12] + ".png";
+function getChineseNewYear(year) {
+    const date = new Date(Lunar.fromYmd(year, 1, 1).getSolar().toString()); // Jan 21 - Feb 20
+    return { date, src: getZodiacAnimal(year), title: '恭喜發財', name: 'Chinese New Year' };
 }
-function isMidAutumnFestival(date) {
-    const sep7 = new Date(date.getFullYear(), 8, 7);
-    const oct9 = new Date(date.getFullYear(), 9, 9);
-    if (date < sep7 || date >= oct9) {
-        return false;
-    }
-    const midAutumn = Lunar.fromYmd(date.getFullYear(), 8, 15).getSolar();
-    return (date.getMonth() + 1) == midAutumn.getMonth() && date.getDate() == midAutumn.getDay();
+function getMidAutumnFestival(year) {
+    const date = new Date(Lunar.fromYmd(year, 8, 15).getSolar().toString()); // Sep 7 - Oct 8
+    return { date, src: '/holidays/mooncake.png', title: '中秋節快樂', name: 'the Mid-Autumn Festival' };
+}
+function getThanksgiving(year) {
+    var date = new Date(today.getFullYear(), 9, 1);
+    date.setDate(date.getDate() + ((8 - date.getDay()) % 7 + 7)); // 2nd Monday (Oct 8 - 14)
+    return { date, src: '/holidays/thanksgiving.png', title: 'Happy Thanksgiving!', name: 'Thanksgiving'};
 }
 var today = new Date();
 //today = new Date(2026, 0, 1); // New Year's Test
+//today = new Date(2026, 1, 7); // 10 days before Chinese New Year Test (Horse)
 //today = new Date(2026, 1, 17); // Chinese New Year's Test (Horse)
 //today = new Date(2026, 6, 1); // Canada Day Test
 //today = new Date(2026, 7, 1); // 1. August Test
@@ -38,59 +33,32 @@ var today = new Date();
 //today = new Date(2026, 9, 31); // Halloween Test
 //today = new Date(2026, 10, 11); // Remembrance Day Test
 //today = new Date(2026, 11, 25); // Christmas Test
-switch (today.getMonth()) {
-    case 0: // January
-        if (today.getDate() === 1) {
-            setLogo('/holidays/fireworks.avif', 'Happy New Year!');
-        } else if (isChineseNewYear(today)) {
-            setLogo(getZodiacAnimal(date), '恭喜發財');
-        }
-        break;
-    case 1: // February
-        if (isChineseNewYear(today)) {
-            setLogo(getZodiacAnimal(today), '恭喜發財');
-        }
-        break;
-    case 6: // July
-        if (today.getDate() === 1) {
-            setLogo('/holidays/Animated-Flag-Canada.gif', 'Happy Canada Day!');
-        }
-        break;
-    case 7: // August
-        if (today.getDate() === 1) {
-            setLogo('/holidays/Animated-Flag-Switzerland.gif', 'En schöne 1. August!');
-        } else if (today.getDate() === 31) {
-            setLogo('/holidays/Animated-Flag-Malaysia.gif', 'Selamat Hari Merdeka!');
-        }
-        break;
-    case 8: // September
-        if (today.getDate() === 16) {
-            setLogo('/holidays/Animated-Flag-Malaysia.gif', 'Selamat Hari Malaysia!');
-        } else if (today.getDate() === 30) {
-            setLogo('/holidays/orange-shirt.png', 'Truth and Reconciliation');
-        } else if (isMidAutumnFestival(today)) {
-            setLogo('/holidays/mooncake.png', '中秋節快樂');
-        }
-        break;
-    case 9: // October
-        var thanksgiving = new Date(today.getFullYear(), 9, 1);
-        thanksgiving.setDate(thanksgiving.getDate() + ((8 - thanksgiving.getDay()) % 7 + 7)); // Calculate 2nd Monday
-        if (today.getDate() === thanksgiving.getDate()) {
-            setLogo('/holidays/thanksgiving.png', 'Happy Thanksgiving!');
-        } else if (today.getDate() === 31) {
-            setLogo('/holidays/pumpkin.gif', 'Happy Halloween!');
-        } else if (isMidAutumnFestival(today)) {
-            setLogo('/holidays/mooncake.png', '中秋節快樂');
-        }
-        break;
-    case 10: // November
-        if (today.getDate() === 11) {
-            setLogo('/holidays/poppy.png', 'Lest We Forget');
-        }
-        break;
-    case 11: // December
-        if (today.getDate() === 25) {
-            setLogo('/holidays/christmas-tree.webp', 'Merry Christmas!');
-        }
-        break;
+//today = new Date(2026, 11, 26); // 6 days to New Year's Test
+const year = today.getFullYear();
+const holiday = [
+    getChineseNewYear(year),
+    getMidAutumnFestival(year),
+    getThanksgiving(year),
+    { date: new Date(year, 0, 1), src: '/holidays/fireworks.avif', title: 'Happy New Year!', name: 'the New Year' },
+    { date: new Date(year, 6, 1), src: '/holidays/Animated-Flag-Canada.gif', title: 'Happy Canada Day!', name: 'Canada Day' },
+    { date: new Date(year, 7, 1), src: '/holidays/Animated-Flag-Switzerland.gif', title: 'En schöne 1. August!', name: '1. August' },
+    { date: new Date(year, 7, 31), src: '/holidays/Animated-Flag-Malaysia.gif', title: 'Selamat Hari Merdeka!', name: 'Malaysia Independence Day' },
+    { date: new Date(year, 8, 16), src: '/holidays/Animated-Flag-Malaysia.gif', title: 'Selamat Hari Malaysia!', name: 'Malaysia Day' },
+    { date: new Date(year, 8, 30), src: '/holidays/orange-shirt.png', title: 'Truth and Reconciliation', name: 'Truth and Reconciliation Day' },
+    { date: new Date(year, 9, 31), src: '/holidays/pumpkin.gif', title: 'Happy Halloween!', name: 'Halloween' },
+    { date: new Date(year, 10, 11), src: '/holidays/poppy.png', title: 'Lest We Forget', name: 'Remembrance Day' },
+    { date: new Date(year, 11, 25), src: '/holidays/christmas-tree.webp', title: 'Merry Christmas!', name: 'Christmas' },
+    { date: new Date(year + 1, 0, 1), src: '/holidays/fireworks.avif', title: 'Happy New Year!', name: 'the New Year' }
+].reduce((closest, current) => {
+    const daysToClosest = daysBetween(today, closest.date);
+    const daysToCurrent = daysBetween(today, current.date);
+    return daysToClosest < 0 || daysToCurrent >= 0 && (daysToCurrent < daysToClosest) ? current : closest;
+});
+const logo = document.getElementById('logo');
+const daysToHoliday = daysBetween(today, holiday.date);
+if (daysToHoliday == 0) {
+    logo.src = holiday.src;
+    logo.title = holiday.title;
+} else {
+    logo.title = daysToHoliday + ' days to ' + holiday.name;
 }
